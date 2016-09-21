8a9e37293688005ad5318236a2b71fd565240f83
mivola <mivola@gmx.de>
2016-09-21 23:28:51 +0200

    fix: dont use session_register & session_start

--- connect_db.php
+++ connect_db.php
@@ -1,34 +1,12 @@
 <?php
 
-  session_start();
+extract($_SESSION); 
 
 $parafile = "para.ini.inc";
-//$para = file($parafile);
 include ($parafile);
 
-//obsolete
-for ($x = 0; $x < count($para); $x++ ) {
-  $str = split("=", $para[$x]);
-  if ($str[0] == "host"){
-//    $host = substr($str[1], 0, -2);
-  }
-  if ($str[0] == "dbname"){
-//    $dbname = substr($str[1], 0, -2);
-  }
-  if ($str[0] == "user"){
-//    $user = substr($str[1], 0, -2);
-  }
-  if ($str[0] == "pass"){
-    //$passwd = $str[1];
-//    $passwd = substr($str[1], 0, -2);
-  }
-  if (($str[0] == "season") && (! isset($season))){
-//    $season = substr($str[1], 0, -1);
-  }
-
-}
-session_register("season");
-session_register("db_name");
+$_SESSION['season']= $season;
+$_SESSION['db_name']= $db_name;
 
 $connect = mysql_connect($db_host, $db_user, $db_passwd) or die ("Verbindung zur Datenbank fehlgeschlagen!");
 
