c0c99319ccfbe92e45a7e9f6b666da96f8514c29
Voigt <mvoigt@eur.ad.sag>
2016-09-21 22:33:01 +0200

    fix: dont use session_register & session_start

--- display_admin.php
+++ display_admin.php
@@ -4,7 +4,7 @@ extract($_SESSION);
 
 require("connect_db.php");
 
-$result = mysql_query("Select * from tbl_user where nick_name = '".$_REQUEST[name]."' and password = '".$_REQUEST[pass]."' and admin <> 0");
+$result = mysql_query("Select * from tbl_user where nick_name = '".$_REQUEST['name']."' and password = '".$_REQUEST['pass']."' and admin <> 0");
 
 require("close_db.php");
 
@@ -22,9 +22,9 @@ while($row = mysql_fetch_array($result)) {
   $table_colB = $row["table_colB"];
   $table_max_points = $row["table_max_points"];
   $phpMySQL = $row["phpMySQL"];
-  session_register("phpMySQL");
+  $_SESSION['phpMySQL']=$phpMySQL;
   $admin = $row["admin"];
-  session_register("admin");
+  $_SESSION['admin']=$admin;
 }
 
 
@@ -48,21 +48,21 @@ extract($_SESSION);
 
   require("close_db.php");
 
-  session_start();
-  session_register("act_userid");
-  session_register("first_name");
-  session_register("last_name");
-  session_register("nick_name");
-  session_register("email");
-  session_register("table_head");
-  session_register("table_lineA");
-  session_register("table_lineB");
-  session_register("table_colA");
-  session_register("table_colB");
-  session_register("table_max_points");
-  session_register("show_long");
-  session_register("phpMySQL");
-  session_register("admin");  
+  //session_start();
+  $_SESSION['act_userid']=$act_userid;
+  $_SESSION['first_name']=$first_name;
+  $_SESSION['last_name']=$last_name;
+  $_SESSION['nick_name']=$nick_name;
+  $_SESSION['email']=$email;
+  $_SESSION['table_head']=$table_head;
+  $_SESSION['table_lineA']=$table_lineA;
+  $_SESSION['table_lineB']=$table_lineB;
+  $_SESSION['table_colA']=$table_colA;
+  $_SESSION['table_colB']=$table_colB;
+  $_SESSION['table_max_points']=$table_max_points;
+  $_SESSION['show_long']=$show_long;
+  $_SESSION['phpMySQL']=$phpMySQL;
+  $_SESSION['admin']=$admin;  
 
   require("top.php");
 
