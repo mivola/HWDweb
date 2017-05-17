e50e683abd86f369dad16514b36ddd6d22b190ed
mivola <mivola@gmx.de>
2017-05-17 07:39:59 +0200

    HWD: add proper icon for Android "Add to Home Screen"

--- top.php
+++ top.php
@@ -1,6 +1,10 @@
 <html>
 <head>
-<title>Willkommen bei HWD</title>
+<title>HWD</title>
+<meta name="viewport" content="width=device-width">
+<meta name="mobile-web-app-capable" content="yes">
+<link rel="icon" sizes="192x192" href="icon.png">
+<link rel="apple-touch-icon" sizes="192x192" href="icon.png">
 <link rel="stylesheet" href="hwd_style.css" type="text/css">
 <script src="js/chkForm.js" type="text/javascript"></script>
 </head>
