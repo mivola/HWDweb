55b72bd98a7305f30522ed8121bf2d50825a7bc0
Michael Voigt <mivola@gmx.de>
2026-07-27 08:50:31 +0200

    update data for season 2026/27

--- user_right.php
+++ user_right.php
@@ -30,11 +30,11 @@ extract($_SESSION);
 	</td></tr></table>
 <?php } else { //if ?>
 
-	Herzlich willkommen bei der Online-Version von HWD zur Saison 2025/2026.</b>
+	Herzlich willkommen bei der Online-Version von HWD zur Saison 2026/2027.</b>
 	<br><br><br>
 	Achtung: Es wird weiterhin die berühmt-berüchtigte "Maximal 4 gleiche Tipps"-Regel überprüft!<br>
 	<br />
-	Die Tipps der vergangenen Saison gibt es wie gewohnt <a href="http://hwd.bts-computer.de/hwd24_25" target="_blank">hier</a>. Auch die <a href="http://hwd.bts-computer.de/hwd-history" target="_blank">HWD-History</a> ist natürlich aktualisiert!<br />
+	Die Tipps der vergangenen Saison gibt es wie gewohnt <a href="http://hwd.bts-computer.de/hwd25_26" target="_blank">hier</a>. Auch die <a href="http://hwd.bts-computer.de/hwd-history" target="_blank">HWD-History</a> ist natürlich aktualisiert!<br />
 	<br />
 	<div align="center">
 	<b>Die HWD-App für Android wurde eingestellt, stattdessen empfehlen wir einfach einen <a href="https://blog.deinhandy.de/internetseite-auf-startbildschirm-hinzufuegen-so-funktionierts-bei-android-und-ios" target="_blank">Shortcut auf dem Startbildschirm zu erstellen!</a>!
